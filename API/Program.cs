17972c065599e228eec10d670edb5781a116b6f0
Roman Sebek <sebekroman2013@gmail.com>
2025-06-19 19:21:24 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
-{opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader()
+       .AllowAnyMethod()
+       .WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
