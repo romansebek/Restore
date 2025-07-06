a8f8038a373a115a7670cb1a0274a51331d2b6b7
Roman Sebek <sebekroman2013@gmail.com>
2025-07-06 11:29:32 +0200

    End of section 8

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,7 +6,7 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { useAppSelector } from "../store/store";
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ScrollRestoration />
       <CssBaseline />
       <NavBar />
       <Box
--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -8,6 +8,7 @@ import { catalogApi } from "../../features/catalog/catalogApi";
 import { uiSlice } from "../layout/uiSlice";
 import { errorApi } from "../../features/about/errorApi";
 import { basketApi } from "../../features/basket/basketApi";
+import { catalogSlice } from "../../features/catalog/catalogSlice";
 
 export function configureTheStore() {
   return legacy_createStore(counterReducer);
@@ -20,6 +21,7 @@ export const store = configureStore({
     [basketApi.reducerPath]: basketApi.reducer,
     counter: counterSlice.reducer,
     ui: uiSlice.reducer,
+    catalog: catalogSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
