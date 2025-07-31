6423ad128224d807aed62208632e252272cbe371
Roman Sebek <sebekroman2013@gmail.com>
2025-07-31 11:51:47 +0200

    Pre publish

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -9,7 +9,7 @@ namespace API.Data;
 
 public class DbInitializer
 {
-public static void InitDb(WebApplication app)
+public static async Task InitDb(WebApplication app)
 {
     using var scope = app.Services.CreateScope();
 
@@ -18,10 +18,10 @@ var context = scope.ServiceProvider.GetRequiredService<StoreContext>()
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>()
     ?? throw new InvalidOperationException("Failed to retrieve user manager");
 
-        SeedData(context, userManager);
+        await SeedData(context, userManager);
 }
 
-    private static async void SeedData(StoreContext context, UserManager<User> userManager)
+    private static async Task SeedData(StoreContext context, UserManager<User> userManager)
     {
         context.Database.Migrate();
 
--- client/src/features/checkout/CheckoutStepper.tsx
+++ client/src/features/checkout/CheckoutStepper.tsx
@@ -22,7 +22,6 @@ import {
   useFetchAddressQuery,
   useUpdateUserAddressMutation,
 } from "../account/accountApi";
-import type { Address } from "../../app/models/user";
 import type {
   ConfirmationToken,
   StripeAddressElementChangeEvent,
@@ -40,8 +39,7 @@ export default function CheckoutStepper() {
   const [activeStep, setActiveStep] = useState(0);
   const [createOrder] = useCreateOrderMutation();
   const { basket } = useBasket();
-  const { data: { name, ...restAddress } = {} as Address, isLoading } =
-    useFetchAddressQuery();
+  const { data, isLoading } = useFetchAddressQuery();
   const [updateAddress] = useUpdateUserAddressMutation();
   const [saveAddressChecked, setSaveAddressChecked] = useState(false);
   const elements = useElements();
@@ -54,10 +52,16 @@ export default function CheckoutStepper() {
   const [confirmationToken, setConfirmationToken] =
     useState<ConfirmationToken | null>(null);
 
+  let name, restAddress;
+  if (data) {
+    ({ name, ...restAddress } = data);
+  }
+
   const handleNext = async () => {
     if (activeStep === 0 && saveAddressChecked && elements) {
       const address = await getStripeAddress();
-      if (address) await updateAddress(address);
+      if (address)
+        await updateAddress({ ...address, postalCode: address.postal_code });
     }
     if (activeStep === 1) {
       if (!elements || !stripe) return;
